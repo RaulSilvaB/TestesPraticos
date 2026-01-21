b123b2abc08c0d994fd8b2077ebf6a55a54f4f31
raul <raulsilvab01@gmail.com>
2026-01-20 21:32:18 -0300

    Routers2

---
+++ 05_fundamentos/src/components/Sobre.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sobre = () => {
+  return (
+    <div>
+      <h2>Sobre a empresa</h2>
+    </div>
+  );
+};
+
+export default Sobre;
---
+++ 05_fundamentos/src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//1 -Roteamentos Básicos
+import Home from "./components/Home.jsx";
+import Contato from "./components/Contato.jsx";
+import Sobre from "./components/Sobre.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contato",
+        element: <Contato />,
+      },
+      {
+        path: "/sobre",
+        element: <Sobre />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
