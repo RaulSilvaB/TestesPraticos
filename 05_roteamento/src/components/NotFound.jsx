3ad0d11f6e36f232e0c2fae7d8c195521966f9f5
raul <raulsilvab01@gmail.com>
2026-01-24 19:20:40 -0300

    roteamentoss

--- 05_roteamento/src/components/NotFound.jsx
+++ 05_roteamento/src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h2>404 - página não encontrada</h2>
+      <h2>404 - página não encontrada 404...</h2>
     </div>
   );
 };
